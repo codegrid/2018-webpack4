10f846aac4488794388b1bea40a14e085ce769ba
tomof <fffjt007@gmail.com>
2018-05-01 17:30:02 +0900

    update 2017-webapck3 4/c-2 to webpack4 base

--- 1/b/webpack.config.js
+++ 1/b/webpack.config.js
@@ -1,16 +1,12 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
     index: './src/index.js',
     about: './src/about.js',
-    vendor: ['react', 'react-dom']
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
   },
   resolve: {
     extensions: ['.js', '.json', '.jsx']
@@ -30,27 +26,29 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: [{
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]'
-            }
-          }, {
-            loader: 'sass-loader',
-          }]
-        })
+        use: [{
+          loader: MiniCssExtractPlugin.loader
+        }, {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[path][name]__[local]--[hash:base64:5]'
+          }
+        }, {
+          loader: 'sass-loader'
+        }]
       }
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      minChunks: Infinity
-    }),
-    new ExtractTextPlugin({
-      filename: '[name].css'
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    }
+  }
 };
